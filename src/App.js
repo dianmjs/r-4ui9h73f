d0e1dcdc7932a02a41a503c52ee709d9174ef781
dianmjs <dianamjs69@gmail.com>
2020-04-21 09:29:44 -0500

    repit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      letler: "",
+    };
+    this.updateInput = this.updateInput.bind(this);
+  }
+
+  updateInput(event) {
+    this.setState({
+      letler: event.target.value,
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input
+          type="text"
+          placeholder="Empieza a escribir algo"
+          value={this.state.letler}
+          onChange={this.updateInput}
+        />
+        <p className="repeater">{this.state.letler}</p>
       </div>
     );
   }
